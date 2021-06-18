7abea598ab293ab2b79136456faa5b3fc2369a8c
intel-raja <itsmeajith96@gmail.com>
2021-06-18 19:33:59 +0530

    gg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:budgetist_kakeibo/SignInScreen.dart';
+import 'package:budgetist_kakeibo/screen/SigninScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: SignInScreen(),
+      home: SigninScreen(),
     );
   }
 }
